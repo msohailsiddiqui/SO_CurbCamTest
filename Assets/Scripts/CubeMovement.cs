4b1bea2b91f3c5b94381aa7b35eb0377c3b67e33
Sohail Siddiqui <sohails@gmail.com>
2015-07-01 12:13:11 +0500

    First Comitt

---
+++ Assets/Scripts/CubeMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeMovement : MonoBehaviour 
+{
+	public float speed;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			transform.Translate(0,0,speed*Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			transform.Translate(0,0,-speed*Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			transform.Translate(-speed*Time.deltaTime,0,0);
+		}
+		
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			transform.Translate(speed*Time.deltaTime,0,0);
+		}
+	
+	}
+}
---
+++ Assets/Scripts/CurbCam.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CurbCam : MonoBehaviour 
+{
+	public Transform targetPosition;
+	public float camWindowDimension;
+
+	Vector2 targetScreenPos;
+	float deltaX;
+	float deltaZ;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//convert target pos to 2D
+		targetScreenPos = Camera.main.WorldToScreenPoint (targetPosition.position);
+
+		if (targetScreenPos.x > (Screen.width/2) + camWindowDimension) 
+		{
+			deltaX = targetScreenPos.x - ((Screen.width/2) + camWindowDimension);
+			transform.position = new Vector3(transform.position.x + deltaX, transform.position.y, transform.position.z);
+		}
+
+		if (targetScreenPos.x < (Screen.width/2) - camWindowDimension) 
+		{
+			deltaX = targetScreenPos.x - ((Screen.width/2) - camWindowDimension);
+			transform.position = new Vector3(transform.position.x + deltaX, transform.position.y, transform.position.z);
+		}
+
+		if (targetScreenPos.y > (Screen.height/2) + camWindowDimension) 
+		{
+			deltaZ = targetScreenPos.y - ((Screen.height/2) + camWindowDimension);
+			transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + deltaZ);
+		}
+
+		if (targetScreenPos.y < (Screen.height/2) - camWindowDimension) 
+		{
+			deltaZ = targetScreenPos.y - ((Screen.height/2) - camWindowDimension);
+			transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + deltaZ);
+		}
+	
+	}
+}
